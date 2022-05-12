61deb6ac21a70c7f48c90e1324b99d479369640d
Laerte Neto <laerte-m@hotmail.com>
2022-05-12 16:26:46 +0100

    test token 2

--- cypress/support/pages/loginPage.js
+++ cypress/support/pages/loginPage.js
@@ -7,7 +7,6 @@ class LoginPage {
   }
 
   assertLoginSucesso() {
-    cy.log('test log')
     cy.get('#flash').click().should('be.visible')
     cy.get('#flash').should('contain.text', 'You logged into a secure area!')
     cy.url().should('include', '/secure')
